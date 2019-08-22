731e2eed61b0373a6130b66b3216536925ec4d8a
Marco Rosa <marccorosa@gmail.com>
2019-08-21 21:49:43 -0300

    b alterou x

--- src/ads3n192_aulagit/Ads3n192_aulagit.java
+++ src/ads3n192_aulagit/Ads3n192_aulagit.java
@@ -17,7 +17,7 @@ public class Ads3n192_aulagit {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int m = 2;
+        int m = 25;
     }
     
 }
